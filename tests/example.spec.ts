6f483a03202c345647e81605d4ed166ee6fd79f5
consultant <test.consultant@diligentkyc.com>
2025-07-25 22:02:11 +0530

    first commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,18 +3,18 @@ import { RuntimeData } from '../utills/utills';
 
 const runtimeData = new RuntimeData();
 
-//const baseUrl = process.env.BASEURL || "http://www.example.com";
+const baseUrl = process.env.BASEURL ;
 
 test('get started link', {tag:'@smoke'},async ({Login}) => {
   //console.log(process.env.BASEURL); 
-  await Login.navigation("http://www.google.com");
+  await Login.navigation(baseUrl);
   await Login.validateElementIsVisible();
   await runtimeData.setData('baseUrl', 'http://www.google.com');
 })
 
 test('get started link222', {tag:'@smoke'},async ({Login}) => {
   //console.log(process.env.BASEURL); 
-  await Login.navigation("http://www.google.com");
+  await Login.navigation(baseUrl);
   await Login.validateElementIsVisible();
   const data=await runtimeData.getData('baseUrl');
   console.log(data); // Should print "http://www.google.com"
--- utills/db.ts
+++ utills/db.ts
@@ -6,24 +6,16 @@ const dbConfig = {
   password: 'your_password',
   connectString: 'localhost/orclpdb1', // or "host:port/service_name"
 };
+ let connection;
 
-async function runQuery() {
-  let connection;
-
+async function connectTODB() {
   try {
     // Create a connection to the database
     connection = await oracledb.getConnection(dbConfig);
     console.log('Successfully connected to Oracle Database');
 
-    // SQL command to execute
-    const result = await connection.execute(
-      `SELECT * FROM employees WHERE ROWNUM <= 5`, // example query
-      [], // bind parameters
-      { outFormat: oracledb.OUT_FORMAT_OBJECT } // returns rows as JS objects
-    );
-
-    console.log('Query Result:', result.rows);
-  } catch (err) {
+  }
+   catch (err) {
     console.error('Error occurred:', err);
   } finally {
     // Always close the connection
@@ -38,4 +30,17 @@ async function runQuery() {
   }
 }
 
-runQuery();
+
+export async function runQuery(SQLQuery: any) {
+  connectTODB();
+    // SQL command to execute
+    const result = await connection.execute(
+      SQLQuery, // example query
+      [], // bind parameters
+      { outFormat: oracledb.OUT_FORMAT_OBJECT } // returns rows as JS objects
+    );
+
+    console.log('Query Result:', result.rows);
+  }
+
+
